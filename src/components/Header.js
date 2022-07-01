94706942fd499c61038f11f5b5152c07ccb2c4f1
rainandray-netizen <ryan.d.adrian.27@gmail.com>
2022-07-01 11:57:07 +0100

    feat: framework comoponents

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './Nav'
+
+const Header = () => {
+  return(
+    <>
+      Header
+      <Nav />
+    </>
+  )
+}
+
+export default Header
---
+++ src/components/Nav.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Nav = () => {
+  return(
+    <>Nav</>
+  )
+}
+
+export default Nav
