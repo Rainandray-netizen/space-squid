a681715dd704f4cae787b74b995ea5c49c80579d
Christine <calyston.14@gmail.com>
2022-07-18 12:17:23 +0100

    portfolio carousel resized

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -9,7 +9,7 @@ const Portfolio = ({ portfolio }) => {
     <article className="portfolio-section">
       <h2>Portfolio</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <article>
+      <article className="portfolio-carousel">
         <Carousel autoPlay={true} infiniteLoop={true} interval={6000}>
           {portfolio.map((project) =>
             <PortfolioTile key={project.id} project={project} />
