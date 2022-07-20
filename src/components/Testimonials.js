91374f7ec629195d23353ae227046e195d466f75
rainandray-netizen <ryan.d.adrian.27@gmail.com>
2022-07-20 11:00:47 +0100

    feat: switched to swiper carousel, completed desktop view portfolio

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,18 +1,18 @@
 import React from 'react'
 
 import TestimonialTile from './TestimonialTile'
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+// import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+// import { Carousel } from 'react-responsive-carousel';
 
 const Testimonials = ({ testimonials }) => {
   return (
     <article className="testimonial-section">
       <h2>What Our Clients Say</h2>
-      <Carousel autoPlay={true} infiniteLoop={true} interval={6000}>
+      {/* <Carousel autoPlay={true} infiniteLoop={true} interval={6000}>
         {testimonials.map((testimonial) =>
           <TestimonialTile key={testimonial.id} testimonial={testimonial} />
         )}
-      </Carousel>
+      </Carousel> */}
     </article>
   )
 }
