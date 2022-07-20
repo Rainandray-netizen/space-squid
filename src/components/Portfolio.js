8d5b390d95ba27bc317893a0d2de82539787e3ab
rainandray-netizen <ryan.d.adrian.27@gmail.com>
2022-07-20 11:38:48 +0100

    feat: added in dynamic width and height detection for slides per view

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import PortfolioTile from './PortfolioTile'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation } from 'swiper'
+import useWindowDimensions from '../hooks/useWindowDimensions'
+
 import 'swiper/css'
 import 'swiper/css/navigation'
 
 const Portfolio = ({ portfolio }) => {
+  const {height, width} = useWindowDimensions()
+  const [swiperSlides, setSwiperSlides] = useState(null)
+
+  useEffect(()=>{
+    let slides = 0
+    if (width >= 1200) { slides = 3 } else
+    if (width >= 768) { slides = 2 } else
+    { slides = 1 }
+    setSwiperSlides(slides)
+  },[width])
 
   return (
     <article className="portfolio-section">
       <h2>Portfolio</h2>
+      <p>height: {height}</p>
+      <p>width: {width}</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <article>
         <Swiper 
           className='mySwiper'
-          slidesPerView={3}
+          slidesPerView={swiperSlides}
           spaceBetween={30}
           navigation={true}
           modules={[Navigation]}
