7837236ffb9d69584333eea440061dc492385898
rainandray-netizen <ryan.d.adrian.27@gmail.com>
2022-07-21 14:11:09 +0100

    feat: added formik to form

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,71 +1,69 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 const ContactUs = ({ contactUs }) => {
-  const { header, body } = contactUs
-  const initialFormData = {
-    name: '',
-    email: '',
-    website: '',
-    message: '',
-    formErrors: { email: '', website: '' },
-    emailValid: false,
-    websiteValid: false,
-    formValid: false,
-  }
-
-  const [formData, setFormData] = useState(initialFormData)
-
-  console.log(formData)
-
-  const handleFormUpdate = (e) => {
-    const name = e.target.name
-    const value = e.target.value
-    setFormData({
-      ...formData,
-      [name]: value
-    })
-  }
-
-  // const validateField = (fieldName, value) => {
-  //   let validationErrors=formData.formErrors
-  //   let emailValid = formData.emailValid
-  //   let websiteValid = formData.websiteValid
-
-  //   switch(fieldName) {
-  //     case 'email':
-  //       emailValid = value.match(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i)
-  //       break
-  //     default:
-  //       break
-  //   }
-  // }
+  const { header, body } = contactUs;
 
   return (
-    <article className="contact-us-section">
-      <h2>{header}</h2>
-      <p>{body}</p>
-      <form>
-        <label>
-          <div>Full Name<span>*</span>:</div>
-          <input onChange={handleFormUpdate} type='text' name='name' value={formData.name} />
-        </label>
+    <Formik
+      initialValues={{ name: '', email: '', website: '', message: '' }}
+      validate={(values) => {
+        const errors = {};
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address';
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <label>
+            <div>
+              Name<span>*</span>:
+            </div>
+            <Field type="text" name="name" />
+            <ErrorMessage name="name" component="p" />
+          </label>
 
-        <label>
-          <div>Email<span>*</span>:</div>
-          <input onChange={handleFormUpdate} type='email' name='email' value={formData.email} />
-        </label>
-        <label>
-          Website:
-          <input onChange={handleFormUpdate} type='text' name='website' value={formData.website} />
-        </label>
-        <label>
-          <div>Message<span>*</span>:</div>
-          <textarea onChange={handleFormUpdate} type='text' name='message' value={formData.message} />
-        </label>
-        <div className="form-button-container"><button>Submit</button></div>
-      </form>
-    </article>
-  )
-}
+          <label>
+            <div>
+              Email<span>*</span>:
+            </div>
+            <Field type="email" name="email" />
+            <ErrorMessage name="email" component="p" />
+          </label>
 
-export default ContactUs
+          <label>
+            <div>Website:</div>
+            <Field type="text" name="website" />
+            <ErrorMessage name="website" component="p" />
+          </label>
+
+          <label>
+            <div>
+              Message<span>*</span>:
+            </div>
+            <Field type="textarea" name="message" />
+            <ErrorMessage name="message" component="p" />
+          </label>
+
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default ContactUs;
